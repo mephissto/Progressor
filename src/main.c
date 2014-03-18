1393b13c4b2ee65511247fb0bc867915b8fe8abb
acymbalski <acymbalski@gmail.com>
2014-03-18 11:02:31 -0700

    Cleaned code, added "am/pm"
    Cleaned up code, got rid of unnecessary definitions and methods, and increased the size of (and re-aligned) the center time font.
    
    In addition, I also added an "am/pm" indicator for the time.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 	
 #define BACKGROUND_COLOR GColorWhite
 #define FOREGROUND_COLOR GColorBlack
-#define CORNER_MASK GCornerNone
-#define CORNER_SIZE 0
+#define ACTUAL_ALIGN GTextAlignmentCenter
+#define AM_PM_ALIGN GTextAlignmentRight
+#define FONT_TINY RESOURCE_ID_FONT_NUMBERS_8		//use sparingly
+#define FONT_SMALL RESOURCE_ID_FONT_NUMBERS_18
+#define FONT_LARGE RESOURCE_ID_FONT_NUMBERS_40
 	
 Window *my_window;
 InverterLayer *layer_hour;
@@ -17,6 +20,7 @@ TextLayer *layer_day_actual;
 TextLayer *layer_week_actual;
 TextLayer *layer_month_actual;
 TextLayer *layer_year_actual;
+TextLayer *layer_am_pm;
 
 static Layer *layer_bar;
 Layer *layer_root;
@@ -28,9 +32,6 @@ static float dayDecimal;
 static float weekDecimal;
 static float monthDecimal;
 static float yearDecimal;
-
-GFont *font_numbers_18;
-GFont *font_numbers_34;
 
 static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed);
 
@@ -53,44 +54,46 @@ void handle_init(void)
 	window_set_fullscreen(my_window, true);
 	window_set_background_color(my_window, BACKGROUND_COLOR);
 	
-	font_numbers_18 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NUMBERS_18));
-	font_numbers_34 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NUMBERS_34));
-	
-	layer_hour = inverter_layer_create(GRect(0, 0, 144, 30));
+	layer_hour = inverter_layer_create(GRect(0, 61, 144, 47));
 	layer_day = inverter_layer_create(GRect(0, 31, 144, 30));
-	layer_week = inverter_layer_create(GRect(0, 61, 144, 47));
-	layer_month = inverter_layer_create(GRect(0, 109, 144, 30));
+	layer_week = inverter_layer_create(GRect(0, 109, 144, 30));
+	layer_month = inverter_layer_create(GRect(0, 0, 144, 30));
 	layer_year = inverter_layer_create(GRect(0, 139, 144, 30));
 	
-	layer_hour_actual	= text_layer_create(GRect(0, 60, 144, 50));
+	layer_hour_actual	= text_layer_create(GRect(0, 58, 144, 50));
 	layer_day_actual	= text_layer_create(GRect(0, 32, 144, 30));
 	layer_week_actual	= text_layer_create(GRect(0, 112, 144, 30));
 	layer_month_actual	= text_layer_create(GRect(0, 2, 144, 30));
 	layer_year_actual	= text_layer_create(GRect(0, 142, 144, 30));
+	layer_am_pm			= text_layer_create(GRect(122, 99, 20, 15));
 	
 	text_layer_set_background_color(layer_hour_actual, GColorClear);
 	text_layer_set_background_color(layer_day_actual, GColorClear);
 	text_layer_set_background_color(layer_week_actual, GColorClear);
 	text_layer_set_background_color(layer_month_actual, GColorClear);
 	text_layer_set_background_color(layer_year_actual, GColorClear);
+	text_layer_set_background_color(layer_am_pm, GColorClear);
 	
-	text_layer_set_text_color(layer_hour_actual, GColorBlack);
-	text_layer_set_text_color(layer_day_actual, GColorBlack);
-	text_layer_set_text_color(layer_week_actual, GColorBlack);
-	text_layer_set_text_color(layer_month_actual, GColorBlack);
-	text_layer_set_text_color(layer_year_actual, GColorBlack);
+	text_layer_set_text_color(layer_hour_actual, FOREGROUND_COLOR);
+	text_layer_set_text_color(layer_day_actual, FOREGROUND_COLOR);
+	text_layer_set_text_color(layer_week_actual, FOREGROUND_COLOR);
+	text_layer_set_text_color(layer_month_actual, FOREGROUND_COLOR);
+	text_layer_set_text_color(layer_year_actual, FOREGROUND_COLOR);
+	text_layer_set_text_color(layer_am_pm, FOREGROUND_COLOR);
 	
-	text_layer_set_font(layer_hour_actual, font_numbers_34);
-	text_layer_set_font(layer_day_actual, font_numbers_18);
-	text_layer_set_font(layer_week_actual, font_numbers_18);
-	text_layer_set_font(layer_month_actual, font_numbers_18);
-	text_layer_set_font(layer_year_actual, font_numbers_18);
+	text_layer_set_font(layer_hour_actual, fonts_load_custom_font(resource_get_handle(FONT_LARGE)));
+	text_layer_set_font(layer_day_actual, fonts_load_custom_font(resource_get_handle(FONT_SMALL)));
+	text_layer_set_font(layer_week_actual, fonts_load_custom_font(resource_get_handle(FONT_SMALL)));
+	text_layer_set_font(layer_month_actual, fonts_load_custom_font(resource_get_handle(FONT_SMALL)));
+	text_layer_set_font(layer_year_actual, fonts_load_custom_font(resource_get_handle(FONT_SMALL)));
+	text_layer_set_font(layer_am_pm, fonts_load_custom_font(resource_get_handle(FONT_TINY)));
 	
-	text_layer_set_text_alignment(layer_hour_actual, GTextAlignmentCenter);
-	text_layer_set_text_alignment(layer_day_actual, GTextAlignmentCenter);
-	text_layer_set_text_alignment(layer_week_actual, GTextAlignmentCenter);
-	text_layer_set_text_alignment(layer_month_actual, GTextAlignmentCenter);
-	text_layer_set_text_alignment(layer_year_actual, GTextAlignmentCenter);
+	text_layer_set_text_alignment(layer_hour_actual, ACTUAL_ALIGN);
+	text_layer_set_text_alignment(layer_day_actual, ACTUAL_ALIGN);
+	text_layer_set_text_alignment(layer_week_actual, ACTUAL_ALIGN);
+	text_layer_set_text_alignment(layer_month_actual, ACTUAL_ALIGN);
+	text_layer_set_text_alignment(layer_year_actual, ACTUAL_ALIGN);
+	text_layer_set_text_alignment(layer_am_pm, AM_PM_ALIGN);
 	
 	layer_root = window_get_root_layer(my_window);
 	layer_bar = layer_create(layer_get_frame(layer_root));
@@ -101,6 +104,7 @@ void handle_init(void)
 	layer_add_child(layer_root, (Layer*) layer_week_actual);
 	layer_add_child(layer_root, (Layer*) layer_month_actual);
 	layer_add_child(layer_root, (Layer*) layer_year_actual);
+	layer_add_child(layer_root, (Layer*) layer_am_pm);
 	
 	layer_add_child(layer_root, (Layer*) layer_hour);
 	layer_add_child(layer_root, (Layer*) layer_day);
@@ -116,16 +120,15 @@ void handle_init(void)
 	layer_mark_dirty(layer_bar);
 	
 	window_stack_push(my_window, true);
-	
 }
 
 void handle_deinit(void)
 {
-	inverter_layer_destroy (layer_hour);
-	inverter_layer_destroy (layer_day);
-	inverter_layer_destroy (layer_week);
-	inverter_layer_destroy (layer_month);
-	inverter_layer_destroy (layer_year);
+	inverter_layer_destroy(layer_hour);
+	inverter_layer_destroy(layer_day);
+	inverter_layer_destroy(layer_week);
+	inverter_layer_destroy(layer_month);
+	inverter_layer_destroy(layer_year);
 	
 	text_layer_destroy(layer_hour_actual);
 	text_layer_destroy(layer_day_actual);
@@ -145,11 +148,25 @@ int main(void) {
 static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed)
 {
 	static char time_text[] = "00:00";
-    char *time_format;
+	char *time_format;
 	if (clock_is_24h_style())
+	{
 		time_format = "%R";
-	else 
+		text_layer_set_text(layer_am_pm, "");
+	}
+	else
+	{
+		static char ampm_text[] = "";
+		strftime(ampm_text, sizeof(ampm_text), "%p", tick_time);
+		if (ampm_text[0] == 'A') {
+			strncpy(ampm_text, "am", sizeof("am"));
+		} else {
+			strncpy(ampm_text, "pm", sizeof("pm"));
+		}
+		text_layer_set_text(layer_am_pm, ampm_text);
+		
 		time_format = "%I:%M";
+	}
 	strftime(time_text, sizeof(time_text), time_format, tick_time);
 	text_layer_set_text(layer_hour_actual, time_text);
 	
@@ -188,6 +205,7 @@ static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed)
 			break;
 	}
 	text_layer_set_text(layer_week_actual, weekday_name);
+	
 	static char month_name[10];
 	strcpy(month_name, "");
 	switch(t->tm_mon)
@@ -233,7 +251,7 @@ static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed)
 			break;
 	}
 	text_layer_set_text(layer_month_actual, month_name);
-		
+	
 	static char year_str[8];
 	strcpy(year_str, "");
 	strcat(year_str, intToString("2000", 8, t->tm_year + 1900));
@@ -244,80 +262,34 @@ static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed)
 	weekDecimal = ((double)((t->tm_wday * 1440) + (t->tm_hour * 60) + t->tm_min) / (double)10080);
 	monthDecimal = ((double)(((t->tm_mday - 1) * 1440) + (t->tm_hour * 60) + t->tm_min) / (double)(daysInMonth(t->tm_mon, t->tm_year) * 1440));
 	yearDecimal = ((double)(((t->tm_yday - 1) * 1440) + (t->tm_hour * 60) + t->tm_min) / (double)(isLeapYear(t->tm_year + 1900) ? 366 * 1440: 365 * 1440));
-	
-	text_layer_update();	
+
 	layer_mark_dirty(layer_bar);
 }
 
 static void bar_layer_draw(Layer *layer, GContext *ctx)
 {
 	graphics_context_set_stroke_color(ctx, FOREGROUND_COLOR);
-	graphics_context_set_fill_color(ctx, FOREGROUND_COLOR);
 	graphics_context_set_text_color(ctx, FOREGROUND_COLOR);
 	
 	//Hour
 	graphics_draw_rect(ctx, GRect(0, 60, 144, 50));
 	layer_set_frame(inverter_layer_get_layer(layer_hour), GRect(1, 61, 142 * hourDecimal, 48));
-	//layer_hour = inverter_layer_create(GRect(0, 60, 144 * hourDecimal, 60));	
-	//graphics_fill_rect(ctx, GRect(0, 16, 144 * hourDecimal, 16), CORNER_SIZE, CORNER_MASK);
 	
 	//Day
 	graphics_draw_rect(ctx, GRect(0, 30, 144, 30));
 	layer_set_frame(inverter_layer_get_layer(layer_day), GRect(1, 31, 142 * dayDecimal, 28));
-	//layer_day = inverter_layer_create(GRect(0, 30, 144 * dayDecimal, 30));
-	//graphics_fill_rect(ctx, GRect(0, 48, 144 * dayDecimal, 16), CORNER_SIZE, CORNER_MASK);
 	
 	//Week
 	graphics_draw_rect(ctx, GRect(0, 110, 144, 30));
 	layer_set_frame(inverter_layer_get_layer(layer_week), GRect(1, 111, 142 * weekDecimal, 28));
-	//layer_week = inverter_layer_create(GRect(0, 110, 144 * weekDecimal, 30));
-	//graphics_fill_rect(ctx, GRect(0, 80, 144 * weekDecimal, 16), CORNER_SIZE, CORNER_MASK);
 	
 	//Month
 	graphics_draw_rect(ctx, GRect(0, 0, 144, 30));
 	layer_set_frame(inverter_layer_get_layer(layer_month), GRect(1, 1, 142 * monthDecimal, 28));
-	//layer_month = inverter_layer_create(GRect(0, 0, 144 * monthDecimal, 30));
-	//graphics_fill_rect(ctx, GRect(0, 112, 144 * monthDecimal, 16), CORNER_SIZE, CORNER_MASK);
 	
 	//Year
 	graphics_draw_rect(ctx, GRect(0, 140, 144, 28));
-	layer_set_frame(inverter_layer_get_layer(layer_year), GRect(1, 141, 142 * yearDecimal, 26));
-	//layer_year = inverter_layer_create(GRect(0, 140, 144 * yearDecimal, 30));
-	//graphics_fill_rect(ctx, GRect(0, 144, 144 * yearDecimal, 16), CORNER_SIZE, CORNER_MASK);
-	
-}
-
-void text_layer_update()
-{
-	//Hour
-	static char percentHour[100];
-	strcpy(percentHour, "");
-	strcat(percentHour, floatToString("100.00%", 7, 100 * hourDecimal));
-	strcat(percentHour, "%");
-	
-	//Day
-	static char percentDay[100];
-	strcpy(percentDay, "");
-	strcat(percentDay, floatToString("100.00%", 7, 100 * dayDecimal));
-	strcat(percentDay, "%");
-	
-	//Week
-	static char percentWeek[100];
-	strcpy(percentWeek, "");
-	strcat(percentWeek, floatToString("100.00%", 7, 100 * weekDecimal));
-	strcat(percentWeek, "%");
-	
-	//Month
-	static char percentMonth[100];
-	strcpy(percentMonth, "");
-	strcat(percentMonth, floatToString("100.00%", 7, 100 * monthDecimal));
-	strcat(percentMonth, "%");
-	
-	//Year
-	static char percentYear[100];
-	strcpy(percentYear, "");
-	strcat(percentYear, floatToString("100.00%", 7, 100 * yearDecimal));
-	strcat(percentYear, "%");
+	layer_set_frame(inverter_layer_get_layer(layer_year), GRect(1, 141, 142 * yearDecimal, 26));	
 }
 
 int daysInMonth(int month, int year)
